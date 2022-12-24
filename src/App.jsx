9545b2c8bd0e5c108005f4976afd667278561069
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-12-24 11:22:40 -0400

    Test SupplierForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import './components/styles.scss'
+import SupplierForm from './components/SupplierForm/SupplierForm';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Hola Mundo
+      <SupplierForm />
     </div>
   )
 }
